13a8ed89235370eb857fc4e3c5a476aff615d78c
leindon Reimoh Dilan <leindonreimohdilan@yahoo.com>
2022-09-26 16:57:16 +0800

    image upload fixed

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, "../public/uploads"));
+  },
+  filename: (req, file, callback) => {
+    const fileName =
+      Date.now() + "_" + file.fieldname + path.extname(file.originalname);
+
+    callback(null, fileName);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+const imageUpload = upload.fields([{ name: "product_image" }]);
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.post("/addproducts", userController.addproducts);
+router.post("/addproducts", imageUpload, userController.addproducts);
 
 module.exports = router;
